59df4a23e5bb4f141d7f08e72c625ab12d365b59
kauareboi <taua.piri@op.ac.nz>
2016-08-30 15:55:15 +1200

    Allow for custom colors to be added to dropdown
    Custom colors can be added to OPColorField that are temporary or belong to one object. At this point custom colors will need to be added to css file.

--- code/OpColorField.php
+++ code/OpColorField.php
@@ -6,18 +6,58 @@ class OpColorField extends DropdownField {
 		$this->addExtraClass('dropdown');
 		$source = ColourSchemes::get()->sort('ID')->map('CSSColor', 'OPColor');
 		parent::__construct($name, $title, $source, $value, $form, $emptyString);
-
-		$this->CurrentTitle = '';
-		if ($value != "") {
-			$star = ColourSchemes::get()->where(array("\"CSSColor\" = '" . $value . "'"))->first();
-			if ($star)
-				$this->CurrentTitle = $star->OPColor;
-		}
-
+        $this->toggleStar();
 		Requirements::css(OPCOLORWORKINGFOLDER . '/css/OpColorField.css');
 		Requirements::javascript(OPCOLORWORKINGFOLDER . '/javascript/OpColorField.js');
 	}
+    
+    /**
+	 * Sets the default color
+	 */
+	public function toggleStar(){
+		if ($this->value != "") {
+			foreach ($this->getSource() as $key => $value) {
+				if($key === $this->value) {
+					$this->CurrentTitle = ($value instanceof Object) ? $value->OPColor : $value;
+				}
+			}
+		}
+	}
 
+	/*
+	 * Sets the source
+	 * @param $source SS_Map
+	 */
+	public function setSource($source) {
+		parent::setSource($source);
+		if(!($source instanceof SS_Map)) {
+			throw new Exception('SS_Map');
+		}
+		$this-> toggleStar();
+		return $this;
+	}
+
+	/**
+	 * Allows for custom colors to be added to source
+	 * eg:	$s = OpColorField::create('ClassOverride', 'Color Override', $this->ClassOverride);
+	 *      $dobj = ArrayData::create(array(
+ 	 *			'OPColor' => 'Black',
+ 	 *			'CSSColor' =>'black',
+ 	 *			'CSSRGB' => '0 0 0',
+ 	 *			'CSSHex' => '#000',
+ 	 *			'CSSCMYK' => '0 0 0 1'
+	 *		));
+	 * 		$s->ammendSource($dobj->CSSColor,$dobj);
+	 * Note: custom colors have to be added to site css file for now.
+	 * @param $key CSS Name $value an array of values to decorate the field
+	 */
+	public function ammendSource($key, $value) {
+		$sources = $this->getSource();
+		$sources->push($key, $value);
+		$this->setSource($sources);
+	}
+
+    
 	/**
 	 * Creates a rendered Programme Crawler Field using the .ss template
 	 * @param type $properties an array of values to decorate the field
@@ -27,25 +67,35 @@ class OpColorField extends DropdownField {
 		$obj = ($properties) ? $this->customise($properties) : $this;
 		$obj->Options = ArrayList::create();
 
-		$dobj = DataObject::create();
-		$dobj->MyTitle = 'No Color';
-		$dobj->Value = '';
-		$dobj->CSSRGB = '255 255 255';
-		$dobj->CSSHex = '#ffffff';
-		$dobj->CSSCMYK = '0 0 0 0';
+		$dobj = DataObject::create(array(
+			'MyTitle' => 'No Color',
+			'Value' =>'',
+			'CSSRGB' => '255 255 255',
+			'CSSHex' => '#ffffff',
+			'CSSCMYK' => '0 0 0 0'
+		));
 		$obj->Options->push($dobj);
 
-		$source = ColourSchemes::get()->sort('ID');
-		if ($source) {
-			foreach ($source as $value) {
-				$mobj = DataObject::create();
+		//go through source, if object it is a custom color, if not, get data from ColourSchemes
+		foreach ($this->getSource() as $value) {
+			$mobj = DataObject::create();
+			if($value instanceof Object){
 				$mobj->MyTitle = $value->OPColor;
 				$mobj->Value = $value->CSSColor;
 				$mobj->CSSRGB = $value->CSSRGB;
 				$mobj->CSSHex = $value->CSSHex;
 				$mobj->CSSCMYK = $value->CSSCMYK;
-				$obj->Options->push($mobj);
+			} else {
+				$cs = ColourSchemes::get()->filter('OPColor', $value)->first();
+				if (!empty($cs)) {
+					$mobj->MyTitle = $cs->OPColor;
+					$mobj->Value = $cs->CSSColor;
+					$mobj->CSSRGB = $cs->CSSRGB;
+					$mobj->CSSHex = $cs->CSSHex;
+					$mobj->CSSCMYK = $cs->CSSCMYK;
+				}
 			}
+			$obj->Options->push($mobj);
 		}
 		
 		// directly point to the template file
